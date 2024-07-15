01347c6d98509d6df53c62f074bfd880f652b957
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-15 12:58:32 +0530

    prepopulate slug field.

--- TrendCo/shop/models.py
+++ TrendCo/shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=100,unique=True)
-    category_slug = models.CharField(max_length=100,unique=True)
+    category_slug = models.SlugField(max_length=100,unique=True)
     category_description = models.TextField(max_length=255,blank=True)
     category_image = models.ImageField(upload_to='category_images/',blank=True)
     
