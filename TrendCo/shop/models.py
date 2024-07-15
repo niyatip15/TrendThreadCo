dd1be3ce3eba542f1aa3a74c42259d8f7103e63e
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-15 12:22:04 +0530

    project setup and custom user model

---
+++ TrendCo/shop/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100,unique=True)
+    category_slug = models.CharField(max_length=100,unique=True)
+    category_description = models.TextField(max_length=255,blank=True)
+    category_image = models.ImageField(upload_to='category_images/',blank=True)
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.category_name
