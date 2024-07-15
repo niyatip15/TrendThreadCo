18577cff3d5a3076a9023f952be205015e12e065
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-15 13:26:09 +0530

    create new model Product

--- TrendCo/store/admin.py
+++ TrendCo/store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Products
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_price', 'product_category', 'in_stock', 'is_available', 'created_date', 'modified_date')
+    list_filter = ('product_category', 'in_stock', 'is_available')
+    search_fields = ('product_name', 'product_desc')
+    prepopulated_fields = {'product_slug': ('product_name',)} 
+     
+admin.site.register(Products, ProductsAdmin)
--- TrendCo/store/models.py
+++ TrendCo/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from product_categories.models import Category
 
 # Create your models here.
+class Products(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    product_slug = models.SlugField(max_length=200,unique=True)
+    product_desc = models.TextField(max_length=500,blank=True)
+    product_price = models.IntegerField()
+    product_images = models.ImageField(upload_to='product_images/')
+    in_stock = models.BooleanField(default=False)
+    is_available = models.BooleanField(default=False)
+    product_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product_name
