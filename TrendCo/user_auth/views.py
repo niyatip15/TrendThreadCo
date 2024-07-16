91f8ecefd1851919b42bc8fcab2028326196f92b
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-16 16:20:32 +0530

    login flow.

--- TrendCo/user_auth/views.py
+++ TrendCo/user_auth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms  import RegistrationForm
 from .models import CustomUser
-from django.contrib import messages
+from django.contrib import messages, auth
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -28,6 +28,17 @@ def register(request):
     return render(request,'user_auth/register.html',context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password = password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request,'Logged in successful')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid credentials')
+            return redirect('login')
     return render(request,'user_auth/login.html')
 
 def logout(request):
