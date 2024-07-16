f2ba23a6dceb90382637c8ddae03118a7ef783c9
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-16 18:55:33 +0530

    bug fixes.

--- TrendCo/cart/urls.py
+++ TrendCo/cart/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.cart,name='cart'),
-    path('add_cart/<slug:product_slug>/', views.add_cart, name='add_cart')
-    
+    path('add_cart/<slug:product_slug>/', views.add_cart, name='add_cart'),
+    path('decrement/<int:cart_item_id>/', views.decrement_cart_item, name='decrement_cart_item'),
 ]
--- TrendCo/cart/views.py
+++ TrendCo/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Products,ProductVariation
-from . models import Cart, CartItems
+from store.models import Products, ProductVariation
+from .models import Cart, CartItems
 
 def _cart_id(request):
     cart_id = request.session.session_key
@@ -11,8 +11,10 @@ def _cart_id(request):
 
 def add_cart(request, product_slug):
     product = get_object_or_404(Products, slug=product_slug)
+    
+    selected_variations = []
+    
     if request.method == 'POST':
-        selected_variations = []
         for item in request.POST:
             key = item
             value = request.POST[key]
@@ -31,61 +33,37 @@ def add_cart(request, product_slug):
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
 
-    is_cart_item_exists = CartItems.objects.filter(cart=cart, product=product).exists()
+    is_cart_item_exists = CartItems.objects.filter(cart=cart, product=product, variations__in=selected_variations).exists()
 
     if is_cart_item_exists:
-        cart_items = CartItems.objects.filter(cart=cart, product=product)
-        
-        # Check if the current variation combination exists
-        existing_variation_list = []
-        id = []
-        for item in cart_items:
-            existing_variations = item.variations.all()
-            existing_variation_list.append(list(existing_variations))
-            id.append(item.id)
-
-        if selected_variations in existing_variation_list:
-            # Increase the quantity of existing item
-            index = existing_variation_list.index(selected_variations)
-            item_id = id[index]
-            item = CartItems.objects.get(product=product, id=item_id)
-            item.quantity += 1
-            item.save()
-        else:
-            # Create a new cart item
-            item = CartItems.objects.create(cart=cart, product=product, quantity=1)
-            if len(selected_variations) > 0:
-                item.variations.clear()
-                item.variations.add(*selected_variations)
-            item.save()
+        cart_item = CartItems.objects.get(cart=cart, product=product, variations__in=selected_variations)
+        cart_item.quantity += 1
+        cart_item.save()
     else:
         cart_item = CartItems.objects.create(
-            product=product,
-            quantity=1,
             cart=cart,
+            product=product,
+            quantity=1
         )
-        if len(selected_variations) > 0:
-            cart_item.variations.clear()
-            cart_item.variations.add(*selected_variations)
+        cart_item.variations.add(*selected_variations)
         cart_item.save()
 
     return redirect('cart')
-
-
 
 def cart(request):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItems.objects.filter(cart=cart, is_active=True)
-        total = 0
-        for cart_item in cart_items:
-            total += (cart_item.product.product_price * cart_item.quantity)
+        total = sum(cart_item.sub_total() for cart_item in cart_items)
         tax = (2 * total) / 100
         grand_total = total + tax
     except Cart.DoesNotExist:
         cart = None
         cart_items = []
         total = 0
+        tax = 0
+        grand_total = 0
+        
     context = {
         'cart_items': cart_items,
         'total': total,
@@ -94,3 +72,14 @@ def cart(request):
     }
 
     return render(request, 'store/cart.html', context)
+
+def decrement_cart_item(request, cart_item_id):
+    cart_item = get_object_or_404(CartItems, id=cart_item_id)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete() 
+
+    return redirect('cart')
