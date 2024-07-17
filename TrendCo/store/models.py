6b5ff0c95c9b3251b7cb9b3affb7158451d66f74
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-17 18:05:29 +0530

    rating average and review count calculation.

--- TrendCo/store/models.py
+++ TrendCo/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from product_categories.models import Category
 from user_auth.models import CustomUser
+from django.db.models import Avg,Count
 # Create your models here.
 class Products(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -13,6 +14,20 @@ class Products(models.Model):
     product_category = models.ForeignKey(Category,on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now_add=True)
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product = self, status = True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+    
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
     
     def __str__(self):
         return self.product_name
