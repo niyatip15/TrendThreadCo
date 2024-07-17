02b2e1d2d740c84bccd77916d33aa8405722a7a6
Niyati Pathak <niyupathak1503@gmail.com>
2024-07-17 18:43:47 +0530

    showing rating star on homepage.

--- TrendCo/TrendCo/views.py
+++ TrendCo/TrendCo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from store.models import Products
+from store.models import Products, ReviewRating
 
 def home(request):
     context = {}
-    products = Products.objects.filter(is_available=True)
+    products = Products.objects.all().filter(is_available=True).order_by('created_date')
+    reviews = None
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True) 
     context['products'] = products
+    context['reviews'] = reviews
     return render(request, 'home.html',context)
